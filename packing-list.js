01e4fc10bc817fd5161b84419549d186bd37b2cd
Shrey Patel <shreypatel11@hotmail.com>
2022-05-18 19:06:01 -0400

    Using while loops to iterate

--- packing-list.js
+++ packing-list.js
@@ -5,3 +5,13 @@ console.log("Kitchen stuff to pack");
 for (let i = 0 ; i < packingList.length ; i++) {
   console.log(packingList[i]);
 }
+
+//---------------------------------
+
+console.log("\nKitchen stuff to pack");
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
